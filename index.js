906136133e258d62afb53c85d4372ab26cb44777
HsiaMin <jackhsia0403@gmail.com>
2023-04-04 17:07:32 +0800

    頁首頁尾RWD

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 $(function () {
   $(".header").load("common.html .header>*", function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 0) {
-        $(".menu_logo a").addClass("logo_on");
-      } else {
-        $(".menu_logo a").removeClass("logo_on");
+    $(window).resize(function () {
+      var width = $(window).width();
+      if (width > 414) {
+        $(window).scroll(function () {
+          if ($(this).scrollTop() > 0) {
+            $(".menu_logo a").addClass("logo_on");
+          } else {
+            $(".menu_logo a").removeClass("logo_on");
+          }
+        });
       }
     });
+
+    // Header_RWD
+    $(".toggle").click(function () {
+      // 導覽列出現
+      $("nav").toggleClass("nav_on");
+      $(this).toggleClass("open");
+    });
+    // 頁尾
+    $(".footer").load("common.html .footer>*");
   });
-  $(".footer").load("common.html .footer>*");
 });
--- product.js
+++ product.js
@@ -2,13 +2,26 @@ $(function () {
   $(".header").load("common.html .header>*", function () {
     // 在加载 header 完成後執行其他操作
     $(".nav_ul li:nth-child(3) a").addClass("active");
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > 0) {
-        $(".menu_logo a").addClass("logo_on");
-      } else {
-        $(".menu_logo a").removeClass("logo_on");
+    $(window).resize(function () {
+      var width = $(window).width();
+      if (width > 414) {
+        $(window).scroll(function () {
+          if ($(this).scrollTop() > 0) {
+            $(".menu_logo a").addClass("logo_on");
+          } else {
+            $(".menu_logo a").removeClass("logo_on");
+          }
+        });
       }
     });
+
+    // Header_RWD
+    $(".toggle").click(function () {
+      // 導覽列出現
+      $("nav").toggleClass("nav_on");
+      $(this).toggleClass("open");
+    });
+    // 頁尾
     $(".footer").load("common.html .footer>*");
   });
 });
